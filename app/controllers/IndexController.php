7e743458dc5520dc91873b0ed382af8fe1034ff8
sulkhan10 <sulkhangalangsakti@gmail.com>
2023-05-04 14:23:38 +0700

    fix : handling cors in index , show , and create

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,6 +1,6 @@
 <?php
-
-
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 use Phalcon\Mvc\Controller;
 
 class IndexController extends Controller
